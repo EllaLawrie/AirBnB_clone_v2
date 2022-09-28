3af1a842dc556c6cd6a94c32e36fd820837c68db
EllaLawrie <ellalawrie002@gmail.com>
2022-09-28 14:25:02 -0700

    clone v2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,7 +5,7 @@ from fabric.api import env
 from fabric.api import put
 from fabric.api import run
 
-env.hosts = ['35.231.216.93', '35.196.0.213']
+env.hosts = ["35.237.92.54", "3.238.90.226"]
 
 
 def do_deploy(archive_path):
